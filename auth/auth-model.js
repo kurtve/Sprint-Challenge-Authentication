f55da14624fa9d6e21807b61f61eb7f7ffbb5bfa
Kurt Van Etten <kurt@vanetten.us>
2020-01-26 19:56:26 -0500

    Add db helper functions to auth-model.js and use this to validate tokens in authenticate-middleware.js

--- auth/auth-model.js
+++ auth/auth-model.js
@@ -1,1 +1,90 @@
 // helper functions for our database model go here
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const db = require('../database/dbConfig.js');
+const secrets = require('../config/secrets.js');
+
+// get a list of all users
+async function find() {
+  return db('users').select('id', 'username');
+}
+
+// get a list of users matching a filter criteria
+function findBy(filter) {
+  return db('users')
+    .where(filter)
+    .select('id', 'username', 'password');
+}
+
+// get a single user with the given id
+async function findById(id) {
+  return db('users')
+    .where({ id })
+    .select('id', 'username', 'password')
+    .first();
+}
+
+// add a user after hashing the user password
+async function add(user) {
+  user.password = await bcrypt.hash(user.password, secrets.HASH_ROUNDS);
+  const [id] = await db('users').insert(user);
+  return findById(id);
+}
+
+// check if a user is valid based on username and password
+async function isValidUser(user) {
+  try {
+    let isValid = false;
+
+    if (user.username && user.password) {
+      const dbUser = await findBy({ username: user.username }).first();
+      isValid = await bcrypt.compare(user.password, dbUser.password);
+    }
+
+    return isValid;
+  } catch (err) {
+    return false;
+  }
+}
+
+// check if a token is valid
+function decodeToken(token) {
+  console.log('inside decodeToken');
+  console.log(token);
+
+  try {
+    const decoded = jwt.verify(token, secrets.JWT_SECRET);
+    console.log(decoded);
+
+    return decoded;
+  } catch (err) {
+    console.log('an error occurred while decoding the token');
+
+    return null;
+  }
+}
+
+// generate a new token for a user
+function makeToken(user) {
+  const payload = {
+    subject: 'user-credentials',
+    username: user.username,
+    role: 'user',
+  };
+
+  const options = {
+    expiresIn: '1d',
+  };
+
+  return jwt.sign(payload, secrets.JWT_SECRET, options);
+}
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+  isValidUser,
+  makeToken,
+  decodeToken,
+};
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,10 +3,16 @@ const authModel = require('./auth-model.js');
 
 const authenticate = async (req, res, next) => {
   try {
-    if (true) {
-      next();
-    } else {
+    const token = req.headers.authorization;
+    let decoded;
+    if (token) {
+      decoded = authModel.decodeToken(token);
+    }
+    if (!decoded) {
       res.status(401).json({ you: 'shall not pass!' });
+    } else {
+      req.username = decoded.username;
+      next();
     }
   } catch (err) {
     next(err);
