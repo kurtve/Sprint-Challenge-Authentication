439a408222d4c34133bf3e5f5c49debdeb6eb6e1
Kurt Van Etten <kurt@vanetten.us>
2020-01-26 19:47:34 -0500

    Make middleware always validate so we can test operation

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,16 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const bcrypt = require('bcryptjs');
+const authModel = require('./auth-model.js');
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+const authenticate = async (req, res, next) => {
+  try {
+    if (true) {
+      next();
+    } else {
+      res.status(401).json({ you: 'shall not pass!' });
+    }
+  } catch (err) {
+    next(err);
+  }
 };
+
+module.exports = authenticate;
